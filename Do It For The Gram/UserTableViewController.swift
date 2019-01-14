90ef3fae5d971b0fd76f06ea968c65adf8cca426
Charles Martin Reed <charlesmartinreed@icloud.com>
2019-01-13 23:36:12 -0600

    Implement query to Parse DB to retrieve all current users. Display current users in the tableview.

--- Do It For The Gram/UserTableViewController.swift
+++ Do It For The Gram/UserTableViewController.swift
@@ -13,9 +13,38 @@ class UserTableViewController: UITableViewController {
 
     //MARK:- Properties
     var currentUser = PFUser.current()
+    var usernames = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Welcome, \(currentUser?.username ?? "Stranger!")")
+        getUsernamesFromParseServer()
+    }
+    
+    func getUsernamesFromParseServer() {
+        //make a user query, initially grabbing them all
+        let query = PFUser.query()
+        
+        //query where to find all the users except for the current one
+        query?.whereKey("username", notEqualTo: currentUser?.username)
+        
+        query?.findObjectsInBackground(block: { (users, error) in
+            if let err = error {
+                self.displayAlert(title: "Uh-oh!", message: err.localizedDescription)
+            } else if let users = users {
+                //unwrap and loop through - note that these are returned as objects, not PFUser
+                users.forEach({ (object) in
+                    if let user = object as? PFUser {
+                        if let username = user.username {
+                            self.usernames.append(username)
+                        }
+                    }
+                })
+                
+                self.tableView.reloadData()
+            }
+        })
     }
 
     // MARK: - Table view data source
@@ -27,13 +56,13 @@ class UserTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 4
+        return usernames.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
         
-        cell.textLabel?.text = currentUser?.username ?? "Username not available"
+        cell.textLabel?.text = usernames[indexPath.row]
         
         return cell
     }
